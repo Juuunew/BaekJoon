9b02def83c0be7a624e469206e5cbfe646400b69
Juuunew <74216712+Juuunew@users.noreply.github.com>
2021-05-26 23:36:40 +0900

    Add files via upload

---
+++ day1_20210526/Cat.java
@@ -0,0 +1,14 @@
+package day1_20210526;
+
+public class Cat {
+
+	public static void main(String[] args) {
+
+		System.out.println( "\\    /\\" );
+		System.out.println( ")  ( ')" );
+		System.out.println( "(  /  )" );
+		System.out.println( " \\(__)|" );
+	}
+
+}
+
